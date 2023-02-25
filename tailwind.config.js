f568b2efa01dfa24f7bf6affdf408c4b9bae942e
yoelvp <yoelvp73@gmail.com>
2023-02-25 18:04:51 -0500

    modify and extend theme for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./resources/views/**/*.blade.php",
-    "./resources/js/**/*.{js,jsx}"
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.{js,jsx}'
   ],
   theme: {
     container: {
