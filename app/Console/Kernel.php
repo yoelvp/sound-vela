8a56dc5ca6a8699c8ba014f78b03e4e06643e4be
yoelvp <yoelvp73@gmail.com>
2023-02-25 21:40:06 -0500

    add task for delete tokens deleted of sanctum

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,23 +5,21 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel
-{
-    /**
-     * Define the application's command schedule.
-     */
-    protected function schedule(Schedule $schedule): void
-    {
-        // $schedule->command('inspire')->hourly();
-    }
+class Kernel extends ConsoleKernel {
+  /**
+   * Define the application's command schedule.
+   */
+  protected function schedule(Schedule $schedule): void {
+    // $schedule->command('inspire')->hourly();
+    $schedule->command('sanctum:prune-expired --hours=24')->daily();
+  }
 
-    /**
-     * Register the commands for the application.
-     */
-    protected function commands(): void
-    {
-        $this->load(__DIR__.'/Commands');
+  /**
+   * Register the commands for the application.
+   */
+  protected function commands(): void {
+    $this->load(__DIR__ . '/Commands');
 
-        require base_path('routes/console.php');
-    }
+    require base_path('routes/console.php');
+  }
 }
