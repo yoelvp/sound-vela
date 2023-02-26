c7388df3a50a68aec085a22995906239a3cfb65c
yoelvp <yoelvp73@gmail.com>
2023-02-25 21:42:10 -0500

    add middleware for routes in api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\auth\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Authentication and register
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   return $request->user();
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+  Route::get('/users', [UserController::class, 'getAllUsers']);
+});
